978b9ae59e21f784aa931a5a5105ac35e916aa9e
LittleYoungBlud <162307055+LittleYoungBlud@users.noreply.github.com>
2024-03-05 02:55:19 -0800

    Update StereoSound.plugin.js

--- StereoSound.plugin.js
+++ StereoSound.plugin.js
@@ -5,7 +5,7 @@
  * @updateUrl https://github.com/LittleYoungBlud/StereoSound/blob/main/StereoSound.plugin.js
  */
 module.exports = (() => {
-    const config = {"main":"index.js","info":{"name":"StereoSound","authors":[{"name":"LittleYoungBlud","discord_id":"1168698907287621713","github_username":"LittleYoungBlud"}],"authorLink":"https://github.com/LittleYoungBlud","version":"0.0.1","description":"Adds a 2 channel input sound to your Better Discord requires a stereo supported mic aka 2 channel microphone.","github":"https://github.com/LittleYoungBlud","github_raw":"https://raw.githubusercontent.com/LittleYoungBlud/StereoSound/main/StereoSound.plugin.js"},"changelog":[{"title":"Changes","items":["This plugin is originally fixed by Riolubruh i just edited some stuff."]}],"defaultConfig":[{"type":"switch","id":"enableToasts","name":"Enable Toasts","note":"Allows the plugin to warn you about voice settings","value":true}]};
+    const config = {"main":"index.js","info":{"name":"StereoSound","authors":[{"name":"LittleYoungBlud","discord_id":"1168698907287621713","github_username":"LittleYoungBlud"}],"authorLink":"https://github.com/LittleYoungBlud","version":"0.0.4","description":"Adds a 2 channel input sound to your Better Discord requires a stereo supported mic aka 2 channel microphone.","github":"https://github.com/LittleYoungBlud","github_raw":"https://raw.githubusercontent.com/LittleYoungBlud/StereoSound/main/StereoSound.plugin.js"},"changelog":[{"title":"Changes","items":["This plugin is originally fixed by Riolubruh i just edited some stuff."]}],"defaultConfig":[{"type":"switch","id":"enableToasts","name":"Enable Toasts","note":"Allows the plugin to warn you about voice settings","value":true}]};
 
     return !global.ZeresPluginLibrary ? class {
         constructor() {this._config = config;}
@@ -94,5 +94,5 @@ module.exports = (() => {
     })(global.ZeresPluginLibrary.buildPlugin(config));
 })();
 
-console.log("young blud")
+
 /*@end@*/
